95b93367d7cbe99c69bf8cb093f29591dd180003
aakashbhat <41189013+aakashbhat@users.noreply.github.com>
2018-10-18 15:05:36 +0200

    Changes made. Works now.
    Efficiency either 91% or 8%

--- fermi_lat_NN_1.py
+++ fermi_lat_NN_1.py
@@ -17,10 +17,6 @@ file_1=fits.open('input3')
 file_2=fits.open('OUTPUT')
 
 #load dataset
-dataDir = r"F:\studies\data\FERMI\\"
-files = listdir(dataDir)
-files.sort()
-totalLength = len(files)
 input_1  = np.empty((1905,5))
 input_1=vstack(file_1[1].data)
 print(input_1.shape)
@@ -43,14 +39,14 @@ for i in range(1905):
 #output_1=to_categorical(output_1)
 
 input_1=np.append(input_1,output_1,axis=1)
-shuffle(input_1)
+np.random.shuffle(input_1)
 test=input_1[:1600,0:5]
 test_truth=input_1[:1600,5:]
 truth_inp=input_1[1600:,:5]
 truth_out=input_1[1600:,5:]
 print(test.shape)
 
-print(input_1)
+#print(input_1)
 
 #model.add(keras.layers.Reshape((1000*5,),input_shape=(5,)))
 #model.add(keras.layers.Conv2D(filters=4,kernel_size=5,strides=(2,2),padding='same',activation=keras.layers.LeakyReLU(alpha=0.2),input_shape=(64,64,3,),data_format="channels_last"))
@@ -62,8 +58,8 @@ print(input_1)
 #model.add(keras.layers.Conv2D(filters=3,kernel_size=10,strides=(2,2),padding='same',activation='relu',data_format="channels_last"))
 
 #model.add(keras.layers.UpSampling1D(size=2,input_shape=(5,)))
-model.add(keras.layers.Conv1D(filters=2,input_shape=(5,),kernel_size=3,strides=1,padding='same',activation='tanh'))
-model.add(keras.layers.Dense(1,activation='tanh'))
+#model.add(keras.layers.Conv1D(filters=2,input_shape=(5,),kernel_size=3,strides=1,padding='same',activation='tanh'))
+model.add(keras.layers.Dense(1,input_shape=(5,),activation='tanh'))
 #model.add(keras.layers.Dense(1,activation='tanh'))
 #model.add(keras.layers.UpSampling2D(size=(2, 2), data_format="channels_last"))
 #model.add(keras.layers.Conv2DTranspose(filters=64,kernel_size=3,strides=1,padding='same',activation = keras.layers.LeakyReLU(alpha=0),data_format="channels_last"))
@@ -83,16 +79,23 @@ model.add(keras.layers.Dense(1,activation='tanh'))
 #model.add(keras.layers.Conv2D(filters=3,kernel_size=5,padding="same",data_format="channels_first"))
 #configure the model
 model.compile(optimizer=tf.train.AdamOptimizer(0.0001),loss='binary_crossentropy', metrics=['accuracy','mae'])
-mod=model.fit(test,test_truth,epochs=10,batch_size=30,validation_split=0.3)
+mod=model.fit(test,test_truth,epochs=20,batch_size=50,validation_split=0.3)
 predictions = model.predict(truth_inp, batch_size=1)
-print(predictions.shape)
+#print(predictions.shape)
 predictions=vstack(predictions)
-print(predictions)
-print(truth_out)
-tru=np.empty((305,1))
+#print(predictions)
+#print(truth_out)
+#predictions_err=predictions-truth_out
+#print(predictions_err)
 #np.savetxt('try.txt',predictions)
 #np.savetxt('try1.txt',truth_out)
-plt.figure(num=None, figsize=(20, 10), dpi=80, facecolor='w', edgecolor='k')
+predictions=np.ravel(predictions)
+truth_out=np.ravel(truth_out)
+print(predictions.shape)
+print(truth_out.shape)
+plt.plot(predictions,truth_out,'o')
+plt.show()
+#plt.figure(num=None, figsize=(20, 10), dpi=80, facecolor='w', edgecolor='k')
 
 #his=mod.history["loss"]
 #plt.plot(his)
