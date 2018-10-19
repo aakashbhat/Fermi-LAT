55006f93b5f3888bcfffd018128d3c2070f369c3
aakashbhat <41189013+aakashbhat@users.noreply.github.com>
2018-10-19 18:30:13 +0200

    Update fermi_lat_NN_1.py

--- fermi_lat_NN_1.py
+++ fermi_lat_NN_1.py
@@ -1,4 +1,5 @@
 
+
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
@@ -35,7 +36,7 @@ model=keras.Sequential()
 for i in range(1905):
     if output_1[i,:]=="AGN":
         output_1[i,:]=1
-    else: output_1[i,:]=-1
+    else: output_1[i,:]=0.5
 #output_1=to_categorical(output_1)
 
 input_1=np.append(input_1,output_1,axis=1)
@@ -59,7 +60,11 @@ print(test.shape)
 
 #model.add(keras.layers.UpSampling1D(size=2,input_shape=(5,)))
 #model.add(keras.layers.Conv1D(filters=2,input_shape=(5,),kernel_size=3,strides=1,padding='same',activation='tanh'))
-model.add(keras.layers.Dense(1,input_shape=(5,),activation='tanh'))
+#model.add(keras.layers.Dropout(rate=0.1,input_shape=(5,)))
+model.add(keras.layers.Dense(5,input_shape=(5,),activation='tanh'))
+model.add(keras.layers.BatchNormalization())
+model.add(keras.layers.Dense(3,activation='tanh'))
+model.add(keras.layers.Dense(1,activation='tanh'))
 #model.add(keras.layers.Dense(1,activation='tanh'))
 #model.add(keras.layers.UpSampling2D(size=(2, 2), data_format="channels_last"))
 #model.add(keras.layers.Conv2DTranspose(filters=64,kernel_size=3,strides=1,padding='same',activation = keras.layers.LeakyReLU(alpha=0),data_format="channels_last"))
@@ -79,7 +84,7 @@ model.add(keras.layers.Dense(1,input_shape=(5,),activation='tanh'))
 #model.add(keras.layers.Conv2D(filters=3,kernel_size=5,padding="same",data_format="channels_first"))
 #configure the model
 model.compile(optimizer=tf.train.AdamOptimizer(0.0001),loss='binary_crossentropy', metrics=['accuracy','mae'])
-mod=model.fit(test,test_truth,epochs=20,batch_size=50,validation_split=0.3)
+mod=model.fit(test,test_truth,epochs=25,batch_size=200,validation_split=0.3)
 predictions = model.predict(truth_inp, batch_size=1)
 #print(predictions.shape)
 predictions=vstack(predictions)
@@ -93,6 +98,7 @@ predictions=np.ravel(predictions)
 truth_out=np.ravel(truth_out)
 print(predictions.shape)
 print(truth_out.shape)
+print(predictions)
 plt.plot(predictions,truth_out,'o')
 plt.show()
 #plt.figure(num=None, figsize=(20, 10), dpi=80, facecolor='w', edgecolor='k')
