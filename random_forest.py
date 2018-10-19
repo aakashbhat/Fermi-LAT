4a9d472b7894dcf8c9aed84edd2877571f785ede
aakashbhat <41189013+aakashbhat@users.noreply.github.com>
2018-10-19 18:25:11 +0200

    added other algos + code for ensemble

--- random_forest.py
+++ random_forest.py
@@ -4,6 +4,12 @@ from random import shuffle, sample
 from astropy.io import fits
 from numpy import vstack
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import VotingClassifier
+from sklearn.model_selection import cross_val_score
 
 file_1=fits.open('input3')
 file_2=fits.open('OUTPUT')
@@ -47,13 +53,54 @@ train_truth=np.ravel(train_truth)
 #test_inp=input_1[1300:1600,:5]
 #test_out=input_1[1300:1600,5:]
 
-clf = RandomForestClassifier(n_estimators=10000,oob_score=True) #n_estimators=no. of trees
+clf = RandomForestClassifier(n_estimators=10000,oob_score=True,random_state=0) #n_estimators=no. of trees
+clf1 = ExtraTreesClassifier(n_estimators=10000,oob_score=True,bootstrap=True,random_state=0)
+clf2 = GradientBoostingClassifier(n_estimators=10000, learning_rate=1.0,max_depth=1, random_state=0).fit(train, train_truth)
+clf3 = AdaBoostClassifier(n_estimators=1000,random_state=0)
+#eclf = VotingClassifier(estimators=[('rf', clf), ('et', clf1), ('gb', clf2),('Ab',clf3)], voting='hard')    #used for the ensemble classification
+
 clf.fit(train,train_truth)                                      #fit model
-print("oob score:")
+clf1.fit(train,train_truth)
+clf3.fit(train,train_truth)
+
+
+'''    use the below part if you want to use an ensemble of the above classifiers
+eclf.fit(train,train_truth)
+for clf, label in zip([clf, clf1, clf2,clf3, eclf], ['Rf', 'Et', 'GB','Adab', 'Ensemble']):
+    scores = cross_val_score(clf, val_inp, val_out, cv=5, scoring='accuracy')
+    print("Accuracy: %0.2f (+/- %0.2f) [%s]" % (scores.mean(), scores.std(), label))
+'''
+print("oob scores(Rf,ET):")
 print(clf.oob_score_)
+print(clf1.oob_score_)
+
 #print(clf.classes_)
 k=clf.feature_importances_
 print("feature importance (flux,unc,signif_curve,spectral_index,var):")
 print(k)
-print("score for validation:")
+print(clf1.feature_importances_)
+print(clf2.feature_importances_)
+print(clf3.feature_importances_)
+print("score for validation(Rf,ET,GB,AdaB,total):")
 print(clf.score(val_inp,val_out))
+print(clf1.score(val_inp,val_out))
+print(clf2.score(val_inp,val_out))
+print(clf3.score(val_inp,val_out))
+#print(eclf.score(val_inp,val_out))
+
+
+'''
+for i in range(len(train_truth)):
+    if train_truth[i]=="AGN":
+        train_truth[i]=1
+    else:
+        train_truth[i]=0
+
+for i in range(len(val_out)):
+    if val_out[i]=="AGN":
+        val_out[i]=1
+    else:
+        val_out[i]=0
+clf4 = LogisticRegression(random_state=0, solver='saga').fit(train, train_truth)
+print(clf4.score(val_inp,val_out))
+'''
