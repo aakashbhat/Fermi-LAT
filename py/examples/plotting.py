2595da4af133a9325a16556dc11ffd2cb36240f2
dm137 <dmitry.malyshev@fau.de>
2019-09-19 23:50:11 +0200

    plottin.py updated

--- py/examples/plotting.py
+++ py/examples/plotting.py
@@ -1,24 +1,34 @@
+# setup figure parameters:
+# import plotting as my_plot
+# my_plot.setup_figure_pars()
 
-# update plotting parameters from a dictionary
-params = {'axes.labelsize': 25, #unit
-          'axes.titlesize': 25,  #title
-          'font.size': 20,
-          'legend.fontsize': 15,
-          'xtick.labelsize':18, #ticks
-          'ytick.labelsize':20,
-          #'text.usetex': True,
-          #'figure.figsize': fig_size,
-          'xtick.major.size' : 6,
-          'ytick.major.size' : 6,
-          'xtick.minor.size' : 3,
-          'ytick.minor.size' : 3,
-          'figure.subplot.left' : 0.05,
-          'figure.subplot.right' : 0.97,
-          'figure.subplot.bottom' : 0.15,
-          'figure.subplot.top' : 0.9
-        }
-pyplot.rcParams.update(params)
+from matplotlib import pyplot
 
-# or directly update individual entries
-pyplot.rcParams['xtick.labelsize'] = 20
+def setup_figure_pars():
+    # update plotting parameters from a dictionary
+    fig_width = 8  # width in inches
+    fig_height = 6    # height in inches
+    fig_size =  [fig_width, fig_height]
+    params = {'axes.labelsize': 25, #unit
+              'axes.titlesize': 25,  #title
+              'font.size': 20,
+              'legend.fontsize': 15,
+              'xtick.labelsize':18, #ticks
+              'ytick.labelsize':20,
+              #'text.usetex': True,
+              'figure.figsize': fig_size,
+              'xtick.major.size' : 6,
+              'ytick.major.size' : 6,
+              'xtick.minor.size' : 3,
+              'ytick.minor.size' : 3,
+              'figure.subplot.left' : 0.05,
+              'figure.subplot.right' : 0.97,
+              'figure.subplot.bottom' : 0.15,
+              'figure.subplot.top' : 0.9
+            }
+    pyplot.rcParams.update(params)
+    return 0
+
+# One can also directly update individual entries
+#pyplot.rcParams['xtick.labelsize'] = 20
 
