d98794e3d80737dfb94f7963808f5e48ef141995
Aakash Bhat <aakashbhat7@gmail.com>
2020-04-24 16:46:47 +0200

    classification

---
+++ ML_3FGL_paper/data/classification.py
@@ -0,0 +1,279 @@
+print(__doc__)
+
+
+# Code source: Gaël Varoquaux
+#              Andreas Müller
+# Modified for documentation by Jaques Grobler
+# License: BSD 3 clause
+import pandas
+from sklearn import preprocessing
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.datasets import make_moons, make_circles, make_classification
+from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+from sklearn.linear_model import LogisticRegression
+
+plt.rcParams['xtick.labelsize'] = 16
+plt.rcParams['axes.labelsize'] = 16
+plt.rcParams['axes.titlesize'] = 25
+plt.rcParams['font.size'] = 15
+plt.rcParams['ytick.labelsize'] = 16
+se=1
+np.random.seed(se)
+dataframe = pandas.read_csv("3fgl_associated_AGNandPSR_final_newindices_withclasses2_nn_allfeat.csv", header=None)
+dataset1 = dataframe.values 
+np.random.shuffle(dataset1[1:])
+#labels2=dataset[:1,:5]
+#print(dataset)
+# split into input (X) and output (Y) variables
+#X = dataset[1:1933,0:5].astype(float)
+X = dataset1[1:,0:10].astype(float)
+print(dataset1[0])
+#Y = dataset[1:1933,5]
+Y = dataset1[1:,10]
+#dataset=dataset[:,:5]
+num=[]
+num2=[]
+resul=[]
+
+weight1=800/166
+weight2=800/1739
+class_weights= {'PSR': weight1, 'AGN': weight2}
+# encode class values as integers
+
+encoder = preprocessing.LabelEncoder()
+encoder.fit(Y)
+Y = encoder.transform(Y)
+
+dataframe = pandas.read_csv("3fgl_unass_withclasses.csv", header=None)
+dataset = dataframe.values
+X2 = dataset[1:,3:9].astype(float)
+Y2 = dataset[1:,11]
+encoder = preprocessing.LabelEncoder()
+encoder.fit(Y2)
+Y2 = encoder.transform(Y2)
+
+
+
+h = .02  # step size in the mesh
+
+names = ["Random Forest", "Neural Net", "AdaBoost"]
+
+classifiers = [RandomForestClassifier(max_depth=12, n_estimators=200, class_weight='balanced',oob_score=True),
+    MLPClassifier(alpha=0.001, max_iter=200),
+    AdaBoostClassifier()]
+
+
+
+
+#X, y = make_classification(n_features=2, n_redundant=0, n_informative=2,
+ #                         random_state=1, n_clusters_per_class=1)
+
+X1=dataset1[1:,2:4]
+print(X1)
+y1=Y
+
+X2=dataset1[1:,2:4]
+y2=Y
+
+X3=dataset1[1:,2:4]
+y3=Y
+#rng = np.random.RandomState(2)
+#X += 2 * rng.uniform(size=X.shape)
+linearly_separable = (X1, y1)
+linearly_separable2 = (X2, y2)
+linearly_separable3 = (X3, y3)
+print(linearly_separable)
+datasets = [#make_moons(noise=0.3, random_state=0),
+            #make_circles(noise=0.2, factor=0.5, random_state=1),
+            linearly_separable2,linearly_separable,linearly_separable3
+            ]
+
+
+X, y = datasets[1]
+X = StandardScaler(with_mean=False,with_std=False).fit_transform(X)
+X_train, X_test, y_train, y_test = \
+ train_test_split(X, y, test_size=.3, random_state=0)
+
+x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
+y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+
+    # just plot the dataset first
+cm = plt.cm.RdBu
+cm_bright = ListedColormap(['#FF0000', '#0000FF'])
+ax1 = plt.subplot()
+    #ax.legend(('Red=AGN', 'Blue=PSR'))
+'''    
+    if ds_cnt == 1:
+        ax.set_title("Input data")
+        ax.set_ylabel("Variability")
+        ax.set_xlabel("Galactic Latitude")
+        #ax.legend(['AGN','PSR'])
+    elif ds_cnt== 0:
+        ax.set_xlabel("Spectral Index")
+        ax.set_ylabel("Significant Curvature")
+        ax.set_xticks(np.arange(0,3,step=0.2))
+        ax.set_yticks(())
+    else :
+        ax.set_xlabel("Flux_Density")
+        ax.set_ylabel("Uncertainity on Energy Flux")
+'''        
+    # Plot the training points
+ax1.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright,
+               edgecolors='k')
+    
+    # Plot the testing points
+ax1.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright, alpha=0.6,
+               edgecolors='k')
+    
+ax1.set_xlim(xx.min(), xx.max())
+ax1.set_ylim(yy.min(), yy.max())
+ax1.set_ylim((-2,5))
+ax1.set_xlabel('Spectral Index')
+ax1.set_ylabel('Log Significant Curvature')
+ax1.set_title('Input Data')
+#ax1.set_xticks(())
+#ax1.set_yticks(())
+plt.show()
+
+
+ax2 = plt.subplot()
+#clf=RandomForestClassifier(max_depth=2, n_estimators=20, class_weight='balanced',oob_score=True)
+#clf= MLPClassifier(max_iter=300,hidden_layer_sizes=(20,5,), activation='tanh', solver='adam',batch_size=1000).fit(X_train,y_train)
+#clf=GradientBoostingClassifier(n_estimators=100, learning_rate=0.3,max_depth=3, random_state=0).fit(X_train,y_train)
+clf=LogisticRegression(max_iter=300, C=0.1,solver='saga',class_weight="balanced").fit(X_train,y_train)
+#clf.fit(X_train, y_train)
+score = clf.score(X_test, y_test)
+        
+        # Plot the decision boundary. For that, we will assign a color to each
+        # point in the mesh [x_min, x_max]x[y_min, y_max].
+if hasattr(clf, "decision_function"):
+    Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
+else:
+    Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
+
+        # Put the result into a color plot
+Z = Z.reshape(xx.shape)
+ax2.contourf(xx, yy, Z, cmap=cm, alpha=.8)
+
+        # Plot the training points
+ax2.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright,
+                   edgecolors='k')
+        # Plot the testing points
+ax2.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright,
+                   edgecolors='k', alpha=0.6)
+
+ax2.set_xlim(xx.min(), xx.max())
+ax2.set_title('Logistic Regression')
+#ax2.set_ylim(yy.min(), yy.max())
+ax2.set_xlabel('Spectral Index')
+ax2.set_ylabel('Log(Significant_Curvature)')
+ax2.set_ylim((-2,5))
+#ax.set_xticks(np.arange(-5,3,step=1))
+ #       ax.set_yticks(())
+  #      if ds_cnt == 0:
+   #         ax.set_title(name)
+ax2.text(xx.max() , yy.max() + .3, ('%.2f' % score).lstrip('0'),
+                size=15, horizontalalignment='right')
+ #       i += 1
+
+        
+figure = plt.figure(figsize=(27, 9))
+figure.suptitle("Classification Domains of Different Algorithms")
+i = 1
+print(datasets[0])
+# iterate over datasets
+for ds_cnt, ds in enumerate(datasets[1:2]):
+    # preprocess dataset, split into training and test part
+    X, y = ds
+    X = StandardScaler(with_mean=False).fit_transform(X)
+    X_train, X_test, y_train, y_test = \
+        train_test_split(X, y, test_size=.3, random_state=42)
+
+    x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
+    y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+
+    # just plot the dataset first
+    cm = plt.cm.RdBu
+    cm_bright = ListedColormap(['#FF0000', '#0000FF'])
+    ax = plt.subplot(len(datasets), len(classifiers) + 1, i)
+    #ax.legend(('Red=AGN', 'Blue=PSR'))
+    
+    if ds_cnt == 1:
+        ax.set_title("Input data")
+        ax.set_ylabel("Variability")
+        ax.set_xlabel("Galactic Latitude")
+        #ax.legend(['AGN','PSR'])
+    elif ds_cnt== 0:
+        ax.set_xlabel("Spectral Index")
+        ax.set_ylabel("Significant Curvature")
+        ax.set_xticks(np.arange(0,3,step=0.2))
+        ax.set_yticks(())
+    else :
+        ax.set_xlabel("Flux_Density")
+        ax.set_ylabel("Uncertainity on Energy Flux")
+    # Plot the training points
+    ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright,
+               edgecolors='k')
+    
+    # Plot the testing points
+    ax.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright, alpha=0.6,
+               edgecolors='k')
+    
+    ax.set_xlim(xx.min(), xx.max())
+    ax.set_ylim(yy.min(), yy.max())
+    ax.set_xticks(())
+    ax.set_yticks(())
+    i += 1
+
+    # iterate over classifiers
+    for name, clf in zip(names, classifiers):
+        ax = plt.subplot(len(datasets), len(classifiers) + 1, i)
+        clf.fit(X_train, y_train)
+        score = clf.score(X_test, y_test)
+
+        # Plot the decision boundary. For that, we will assign a color to each
+        # point in the mesh [x_min, x_max]x[y_min, y_max].
+        if hasattr(clf, "decision_function"):
+            Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
+        else:
+            Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
+
+        # Put the result into a color plot
+        Z = Z.reshape(xx.shape)
+        ax.contourf(xx, yy, Z, cmap=cm, alpha=.8)
+
+        # Plot the training points
+        ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright,
+                   edgecolors='k')
+        # Plot the testing points
+        ax.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright,
+                   edgecolors='k', alpha=0.6)
+
+        ax.set_xlim(xx.min(), xx.max())
+        ax.set_ylim(yy.min(), yy.max())
+        ax.set_xticks(np.arange(-5,3,step=1))
+        ax.set_yticks(())
+        if ds_cnt == 0:
+            ax.set_title(name)
+        ax.text(xx.max() , yy.max() + .3, ('%.2f' % score).lstrip('0'),
+                size=15, horizontalalignment='right')
+        i += 1
+
+plt.tight_layout()
+plt.show()
