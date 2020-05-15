e10f7983bc2a595dbc35f4b3f3a3972b5e990808
Aakash Bhat <aakashbhat7@gmail.com>
2020-05-15 14:10:45 +0200

    new classification file

--- ML_3FGL_paper/data/classification.py
+++ ML_3FGL_paper/data/classification.py
@@ -24,20 +24,20 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 from sklearn.linear_model import LogisticRegression
 
-plt.rcParams['xtick.labelsize'] = 16
-plt.rcParams['axes.labelsize'] = 16
-plt.rcParams['axes.titlesize'] = 25
-plt.rcParams['font.size'] = 15
-plt.rcParams['ytick.labelsize'] = 16
+
+plt.rcParams['xtick.labelsize'] = 22
+plt.rcParams['axes.labelsize'] = 22
+plt.rcParams['axes.titlesize'] = 28
+plt.rcParams['font.size'] = 23
+plt.rcParams['ytick.labelsize'] = 22
+
+
+#Training Fata:
 se=1
 np.random.seed(se)
 dataframe = pandas.read_csv("3fgl_associated_AGNandPSR_final_newindices_withclasses2_nn_allfeat.csv", header=None)
 dataset1 = dataframe.values 
 np.random.shuffle(dataset1[1:])
-#labels2=dataset[:1,:5]
-#print(dataset)
-# split into input (X) and output (Y) variables
-#X = dataset[1:1933,0:5].astype(float)
 X = dataset1[1:,0:10].astype(float)
 print(dataset1[0])
 #Y = dataset[1:1933,5]
@@ -56,6 +56,7 @@ encoder = preprocessing.LabelEncoder()
 encoder.fit(Y)
 Y = encoder.transform(Y)
 
+#Testing Data, although we aren't using it at the moment since we are more concerned with the classfication domains of the training data:
 dataframe = pandas.read_csv("3fgl_unass_withclasses.csv", header=None)
 dataset = dataframe.values
 X2 = dataset[1:,3:9].astype(float)
@@ -68,9 +69,12 @@ Y2 = encoder.transform(Y2)
 
 h = .02  # step size in the mesh
 
+
+
+#Much of the code was taken from elsewhere so names and classifiers function below, but we don't need it at the moment
 names = ["Random Forest", "Neural Net", "AdaBoost"]
 
-classifiers = [RandomForestClassifier(max_depth=12, n_estimators=200, class_weight='balanced',oob_score=True),
+classifiers = [RandomForestClassifier(max_depth=200, n_estimators=12, class_weight='balanced',oob_score=True),
     MLPClassifier(alpha=0.001, max_iter=200),
     AdaBoostClassifier()]
 
@@ -80,6 +84,7 @@ classifiers = [RandomForestClassifier(max_depth=12, n_estimators=200, class_weig
 #X, y = make_classification(n_features=2, n_redundant=0, n_informative=2,
  #                         random_state=1, n_clusters_per_class=1)
 
+#Making datsets, these were initially with different features and not the same:
 X1=dataset1[1:,2:4]
 print(X1)
 y1=Y
@@ -89,6 +94,8 @@ y2=Y
 
 X3=dataset1[1:,2:4]
 y3=Y
+
+
 #rng = np.random.RandomState(2)
 #X += 2 * rng.uniform(size=X.shape)
 linearly_separable = (X1, y1)
@@ -101,12 +108,14 @@ datasets = [#make_moons(noise=0.3, random_state=0),
             ]
 
 
+
+
 X, y = datasets[1]
 X = StandardScaler(with_mean=False,with_std=False).fit_transform(X)
 X_train, X_test, y_train, y_test = \
- train_test_split(X, y, test_size=.3, random_state=0)
+ train_test_split(X, y, test_size=.3, random_state=0)       #Split into training and validation
 
-x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
+x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5   #Define minimumm and maximum of axes
 y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
 xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
                          np.arange(y_min, y_max, h))
@@ -131,7 +140,7 @@ ax1 = plt.subplot()
         ax.set_xlabel("Flux_Density")
         ax.set_ylabel("Uncertainity on Energy Flux")
 '''        
-    # Plot the training points
+    # Plot the training points for input plot, Not really necessary
 ax1.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright,
                edgecolors='k')
     
@@ -149,14 +158,19 @@ ax1.set_title('Input Data')
 #ax1.set_yticks(())
 plt.show()
 
+#Main part of code:
 
 ax2 = plt.subplot()
-#clf=RandomForestClassifier(max_depth=2, n_estimators=20, class_weight='balanced',oob_score=True)
-#clf= MLPClassifier(max_iter=300,hidden_layer_sizes=(20,5,), activation='tanh', solver='adam',batch_size=1000).fit(X_train,y_train)
+
+#Choose classifier:
+#clf=RandomForestClassifier(max_depth=4, n_estimators=200, class_weight='balanced',oob_score=True)
+clf= MLPClassifier(max_iter=300,hidden_layer_sizes=(), activation='identity', solver='sgd',batch_size=1000).fit(X_train,y_train)
 #clf=GradientBoostingClassifier(n_estimators=100, learning_rate=0.3,max_depth=3, random_state=0).fit(X_train,y_train)
-clf=LogisticRegression(max_iter=300, C=0.1,solver='saga',class_weight="balanced").fit(X_train,y_train)
-#clf.fit(X_train, y_train)
-score = clf.score(X_test, y_test)
+#clf=LogisticRegression(max_iter=300, C=0.1,solver='saga',class_weight="balanced").fit(X_train,y_train)
+clf.fit(X_train, y_train)
+
+
+score = clf.score(X_test, y_test)       #Score of our classifier
         
         # Plot the decision boundary. For that, we will assign a color to each
         # point in the mesh [x_min, x_max]x[y_min, y_max].
@@ -170,14 +184,20 @@ Z = Z.reshape(xx.shape)
 ax2.contourf(xx, yy, Z, cmap=cm, alpha=.8)
 
         # Plot the training points
-ax2.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright,
-                   edgecolors='k')
+scatter1=ax2.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright,edgecolors='k')
         # Plot the testing points
-ax2.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright,
-                   edgecolors='k', alpha=0.6)
+scatter2=ax2.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright, alpha=0.6,edgecolors='k')
+
+#Make legends:
+k=scatter1.legend_elements()[0]
+k2=scatter2.legend_elements()[0]
+k3=k+k2
+k3=np.asarray(k3)
+legend1 = ax2.legend(k3,('Train AGN', 'Train PSR','Test AGN','Test PSR'),loc='upper right',fontsize=16)
+
 
 ax2.set_xlim(xx.min(), xx.max())
-ax2.set_title('Logistic Regression')
+ax2.set_title('Random Forest (200,4)')
 #ax2.set_ylim(yy.min(), yy.max())
 ax2.set_xlabel('Spectral Index')
 ax2.set_ylabel('Log(Significant_Curvature)')
@@ -190,7 +210,7 @@ ax2.text(xx.max() , yy.max() + .3, ('%.2f' % score).lstrip('0'),
                 size=15, horizontalalignment='right')
  #       i += 1
 
-        
+'''        
 figure = plt.figure(figsize=(27, 9))
 figure.suptitle("Classification Domains of Different Algorithms")
 i = 1
@@ -274,6 +294,6 @@ for ds_cnt, ds in enumerate(datasets[1:2]):
         ax.text(xx.max() , yy.max() + .3, ('%.2f' % score).lstrip('0'),
                 size=15, horizontalalignment='right')
         i += 1
-
+'''
 plt.tight_layout()
 plt.show()
