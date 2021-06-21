f81393e57c6f3a82a8da38a3a036d8d0031a7a42
Aakash Bhat <aakashbhat7@gmail.com>
2021-06-21 23:27:24 +0200

    new appendix

--- ML_3FGL_paper_extra/data/python files/histogram.py
+++ ML_3FGL_paper_extra/data/python files/histogram.py
@@ -5,10 +5,10 @@ plt.rcParams['axes.labelsize'] = 28
 plt.rcParams['axes.titlesize'] = 28
 plt.rcParams['font.size'] = 28
 plt.rcParams['ytick.labelsize'] = 28
-data = pd.read_csv('./catas/4FGLDR2comparison.csv', quoting=2,usecols=['diffLR2'])
+data = pd.read_csv('./catas/4FGLDR2comparison_3CLASS.csv', quoting=2,usecols=['diffLR'])
 
 data.hist(bins=50)
-plt.title("Logistic regression, 4FGL-DR2 (2-class)")
+plt.title("Logistic regression, 4FGL-DR2 (3-class)")
 plt.xlabel(xlabel=r'$\frac{P_O-P_S}{max(\sigma_O,\sigma_S)}$',fontsize=34)
 plt.xlim(-3,3)
 #plt.ylabel("Frequency")
