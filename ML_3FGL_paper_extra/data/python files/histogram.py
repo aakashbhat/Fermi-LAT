785b2c074027494d552aa2d1cb662f8d1f877ce8
Aakash Bhat <aakashbhat7@gmail.com>
2022-01-11 18:05:32 +0100

    removed grid in histogramremoved grid in histograms

--- ML_3FGL_paper_extra/data/python files/histogram.py
+++ ML_3FGL_paper_extra/data/python files/histogram.py
@@ -5,12 +5,13 @@ plt.rcParams['axes.labelsize'] = 28
 plt.rcParams['axes.titlesize'] = 28
 plt.rcParams['font.size'] = 28
 plt.rcParams['ytick.labelsize'] = 28
-data = pd.read_csv('./catas/4FGL-DR2_2Class_Catalog_AllSamplings.csv', quoting=2,usecols=['diffPSR_LR'])
+data = pd.read_csv('./catas/3FGL_3Class_Catalog_AllSamplings.csv', quoting=2,usecols=['diffPSR_LR'])
 
 data.hist(bins=50)
-plt.title("Logistic regression, 4FGL-DR2, PSR-like probabilities (2-class)")
+plt.title("Logistic regression, 3FGL, PSR-like probabilities (3-class)")
 plt.xlabel(xlabel=r'$\frac{P_O-P_S}{max(\sigma_O,\sigma_S)}$',fontsize=34)
 plt.xlim(-3,3)
+plt.grid(False)
 #plt.ylabel("Frequency")
 plt.show()
 
